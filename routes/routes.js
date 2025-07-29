b32e6f4e49eb600799af152f5eb1631c3262d5b1
Krishna Murari <128674792+murari08@users.noreply.github.com>
2025-07-29 15:29:03 +0530

    Create routes.js

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const userController = require('../controllers/userControllers')
+const loginController = require('../controllers/login')
+const {verifyToken,verifyAuthToken,roleBasedAccess} = require('../middleware/middleware')
+const router = express.Router();
+
+
+
+router.post('/signup',loginController.signup)
+router.post('/login',loginController.login)
+router.post('/addpost',verifyAuthToken,roleBasedAccess('admin','editor'),userController.addPost);
+router.post('/addlogs',verifyToken,userController.addLog)
+router.get('/getlogs',verifyAuthToken,roleBasedAccess('admin'),userController.getLogs)
+router.get('/getpost',verifyAuthToken,roleBasedAccess('admin','editor','viewer'),userController.getPost)
+router.get('/getlogdetails',verifyAuthToken,roleBasedAccess('admin'),userController.loginDetails)
+
+
+
+module.exports = router
+
